f8156406d75dc54a004207f7627a93a20a2a2dc7
Ugly_Betty🙈 <macbookair@MACBOOKs-MacBook-Pro.local>
2022-08-14 21:22:31 +0100

    v1

---
+++ script.js
@@ -0,0 +1,120 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
+
+// Show input Error message
+function showError(input, message) {
+    const formControl = input.parentElement 
+    formControl.className = 'form-control error'
+    const small = formControl.querySelector('small')
+    small.innerText = message
+}
+
+// Show success outline
+function showSuccess(input) {
+    const formControl = input.parentElement
+    formControl.className = 'form-control success'
+}
+
+
+// Check email is valid
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\‌​.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
+    if(re.test(input.value.trim())) {
+        showSuccess(input)
+    } else {
+        showError(input, 'Email is not valid')
+    }
+}
+
+
+// Check password match
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Password do not match')
+    }
+}
+
+// Check password format
+function checkPasswordFormat(input) {
+    const passformat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    if (input.value.match(passformat)) {
+        showSuccess(input)
+        return true
+    } else {
+        showError(input, 'Must contain at least 6 characters, 1 uppercase and number!!!')
+        return false
+    }
+}
+
+
+// Check required fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function (input) {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`)
+        } else {
+            showSuccess(input)
+        }
+    })
+}
+
+
+// Check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min ) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    } else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+    } else {
+        showSuccess(input)
+    }
+}
+
+
+// Get Field
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
+
+// Event listener 
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    // if (username.value === '') {
+    //     showError(username, 'Username is required')
+    // } else {
+    //     showSuccess(username)
+    // }
+
+    // if (email.value === '') {
+    //     showError(email, 'Email is required')
+    // } else if (!isValidEmail(email.value)) {
+    //     showError(email, 'Email is not Valid')
+    // }
+    // else {
+    //     showSuccess(email)
+    // }
+
+    // if (password.value === '') {
+    //     showError(password, 'Password is required')
+    // } else {
+    //     showSuccess(password)
+    // }
+
+    // if (password2.value === '') {
+    //     showError(password2, 'Password 2 is required')
+    // } else {
+    //     showSuccess(password2)
+    // }
+
+    checkRequired([username, email, password, password2])
+    checkLength(username, 3, 15)
+    checkLength(password, 6, 25)
+    checkEmail(email)
+    checkPasswordsMatch(password, password2)
+    checkPasswordFormat(password)
+})
+
